b87665223e0eefd1baf8b3fadfdd384591ceb853
hhuezo <hugo.alex.huezo@gmail.com>
2023-11-22 18:53:45 -0600

    test

--- app/Http/Controllers/api/ApiSolicitudController.php
+++ app/Http/Controllers/api/ApiSolicitudController.php
@@ -116,8 +116,9 @@ class ApiSolicitudController extends Controller
 
         $max_numero = Solicitud::max('Numero');
         $max_credito = Solicitud::max('NumeroCredito');
+        $data = $request->json()->all();
 
-        return response()->json(['max_numero' => $max_numero, 'max_credito' => $request ]);
+        return response()->json(['max_numero' => $max_numero, 'max_credito' => $data ]);
         /*$messages = [
 
             'Fecha.required' => 'Fecha es un valor requerido',
